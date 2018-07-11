2cdce6ec985ea6fc80b8b26fab14dea4e3f46be6
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-07-11 18:38:52 -0500

    intial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || "8080";
+
+
+
+const namesArr = ["Collin", "Slim Shady", "Dr Dre", "Ez", "Mark Wahlberg", "Bill Murray", "Rob Reiner", "Ryan Reynolds", "Spinal Tap"];
+
+const posArr = ["Missionary", "Washed Up", "Fuckin Rad"];
+
+function Player(name, pos, off, def){
+    this.name = name;
+    this.position = pos;
+    this.offense = off;
+    this.defense = def;
+}
+
+let playersArray = [];
+
+for(let i = 0; i < namesArr.length; i++){
+    let player = new Player(
+        namesArr[i],
+        posArr[Math.floor(Math.random() * posArr.length)],
+        Math.floor(Math.random() * 10) + 1,
+        Math.floor(Math.random() * 10) + 1,
+
+    );
+
+    playersArray.push(player);
+}
+
+app.get("/api/players", (req, res) => {
+    res.json(playersArray);
+});
+
+
+
+app.listen(PORT, () => {
+    console.log("Server is listening on port: " + PORT);
+});
